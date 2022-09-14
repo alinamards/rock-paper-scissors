9cba61492a88b0d2c74dfb616de8e6759c01e18d
Alina Mardanova <mardanova9@gmail.com>
2022-09-14 19:31:56 +0300

    Write algorithm to play 5 rounds of game

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
+let computerScore = 0;
+let playerScore = 0;
+
 function getComputerChoice() {
-    let list = ["Rock", "Paper", "Scissors"];
+    const list = ["Rock", "Paper", "Scissors"];
     let randomChoiceComputer = list[Math.floor(Math.random() * list.length)];
-    console.log(randomChoiceComputer);
+    return randomChoiceComputer;
 }
+
+function getPlayerChoice() {
+    let playerChoice = prompt("What is your choice?");
+    return playerChoice;
+} 
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = getPlayerChoice().toLowerCase();
+    computerSelection = getComputerChoice().toLowerCase();
+
+    if (computerSelection == playerSelection) {
+        console.log("It's a tie!");
+    } else if (
+        (computerSelection == "rock" && playerSelection == "scissors") ||
+        (computerSelection == "scissors" && playerSelection == "paper") ||
+        (computerSelection == "paper" && playerSelection == "rock")
+    ) {
+        computerScore = ++computerScore;
+        console.log(`You lost! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}.`
+        );
+    } else {
+        playerScore = ++playerScore;
+        console.log(`You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}.`
+        );
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    
+    if (playerScore > computerScore) {
+        console.log("You won!");
+    } else if (computerScore > playerScore) {
+        console.log("You lost!");
+    } else {
+        console.log("It's a tie...");
+    }
+    
+}
+
